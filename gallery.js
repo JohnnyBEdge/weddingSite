be926d0867cc2dc41e83ac71d5fe14f6860e3df0
John Martin <jrmartin182@gmail.com>
2019-04-10 17:02:40 -0600

    added cuntion to close image with click outside of it

--- gallery.js
+++ gallery.js
@@ -18,8 +18,12 @@ const items = document.querySelectorAll('.item');
 items.forEach(item => item.addEventListener('click', handleClick));
 overlayClose.addEventListener('click', close);
 
+
+// closes image with esc
 window.onkeyup = function (event) {
   if (event.keyCode == 27) {
    window.close();
   }
  }
+//close image with click outside of image
+ overlay.addEventListener('click',close)
